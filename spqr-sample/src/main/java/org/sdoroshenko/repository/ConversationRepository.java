f7f19a641ac8cbed3afdcc3e38007c167a3b7e98
sdoroshenko <sdoroshenko@edmunds.com>
2018-05-25 18:27:31 +0300

    Custom error handler

--- spqr-sample/src/main/java/org/sdoroshenko/repository/ConversationRepository.java
+++ spqr-sample/src/main/java/org/sdoroshenko/repository/ConversationRepository.java
@@ -1,7 +1,6 @@
 package org.sdoroshenko.repository;
 
 import org.sdoroshenko.model.Conversation;
-import org.sdoroshenko.model.Message;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ConversationRepository extends CrudRepository<Conversation, Long> {
